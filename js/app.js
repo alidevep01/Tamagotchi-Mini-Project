0a8b6fb1b666a1c618acb4cae5eebd9092739c32
mohdali0326 <mohammadali0326@gmail.com>
2022-07-07 10:58:34 -0500

    started with javascript

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Create an Object or Class (look at your notes on JS Classes if you forgot) for your Tamagotchi
+// Display the following metrics for your pet:
+
+// Hunger (1-10 scale)
+// Sleepiness (1-10 scale)
+// Boredom (1-10 scale)
+// Age
+
+// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+
+// Add the ability to name your pet.
+// Increase your pet's age every x minutes
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Morph your pet at certain ages.
+// Animate your pet across the screen while it's alive.
+
+
+
