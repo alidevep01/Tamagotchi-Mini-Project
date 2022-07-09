942f8456a426d36910d72f334e57308cc0ac981d
mohdali0326 <mohammadali0326@gmail.com>
2022-07-08 23:08:28 -0500

    created sleepUp, hungry, bored methods

--- js/app.js
+++ js/app.js
@@ -1,30 +1,146 @@
-// Create an Object or Class (look at your notes on JS Classes if you forgot) for your Tamagotchi
-// Display the following metrics for your pet:
+//Create a pet class with following arguments
+//Name, age, sleep, hunger, boredom
 
-// Hunger (1-10 scale)
-// Sleepiness (1-10 scale)
-// Boredom (1-10 scale)
-// Age
+class Pet {
+    constructor(name, age, sleep, hunger, boredom) {
+        this.name = name;
+        this.age = age;
+        this.sleep = sleep;
+        this.hunger = hunger;
+        this.boredom = boredom;
+    }
 
-// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
 
-// Add the ability to name your pet.
-// Increase your pet's age every x minutes
-// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
-// Morph your pet at certain ages.
-// Animate your pet across the screen while it's alive.
+}
+//create pokemon as a child of pet
+//create method for age, sleep hunger and boredom when called decrement by 1
+//create a name method, if no name given display guest
+class Pokemon extends Pet {
+    constructor(name, age, sleep, hunger, boredom) {
+        super(name, age, sleep, hunger, boredom)
+        this.name = name;
+        this.age = 0;
+        this.sleep = 0;
+        this.hunger = 0;
+        this.boredom = 0;
+    }
+    sleep() {
+        this.sleep--
+    }
+    hunger() {
+        this.hunger--
+    }
+    boredom() {
+        this.boredom--
+    }
+    name() {
+        if (this.name = "") {
+            let guest = "Guest"
+            return this.name = guest
+        } else {
+            return this.name
+        }
 
+    }
+
+}
+
+let pokemon = new Pokemon("Tamagotchi")
+let lightsOn = true;
 
 /* ***************************************** Modal Section Start ********************************************* */
 
 //on page load open the modal-box by changing diplay to flex
 window.addEventListener("load", () => {
     document.querySelector(".modal-box").style.display = "flex";
-  
 })
 
+const tamagotchi = {
+    ageUp() {
+        setInterval(() => {
+            document.getElementById("age").innerText = `Age: ${pokemon.age}`
+            document.getElementById("age").style.width = pokemon.age + "%";
+            pokemon.age++
+        }, 1000)
+    },
 
+    //when clicked on lights on/off button toggle video src to lights-on/out video according
+    //when clicked on lights on/off descrease sleep progress-bar by one
+    //when not-clicked on lights on/off increase sleep progress-bar by one
+    //when progress bar reached 10 kill pet(pokemon) pause video Show the result
+
+    /* ***** Lights on/off (sleep)***** */
+    lightsOnOff() {
+        lightsOn = !lightsOn;
+        if (lightsOn) {
+            document.querySelector("#myVideo").src = "./images/lights-off.mp4"
+        } else {
+            document.querySelector("#myVideo").src = "./images/lights-on.mp4"
+        }
+    },
+    sleepUp() {
+        setInterval(() => {
+            document.getElementById("sleep").innerText = `Sleep: ${Math.ceil(pokemon.sleep /100 * 10)}`
+            document.getElementById("sleep").style.width = pokemon.sleep + "%";
+            pokemon.sleep++
+        }, 900)
+    },
+
+    //when clicked on feed me decrease eat progress-bar by one
+    //when not clicked on feed me increase progress-bar eat by one
+    //when eat progress bar reaches 10 kill pet(pokemon) pause video Show the result
+
+    /* ***** Feed Me(Eat) ***** */
+    hungry() {
+        setInterval(() => {
+                document.getElementById("eat").innerText = `eat: ${Math.ceil(pokemon.hunger /100 * 10)}`
+                document.getElementById("eat").style.width = pokemon.hunger + "%";
+                pokemon.hunger++
+            }, 800),
+            document.querySelector(".feedMeBtn").addEventListener("click", () => {
+                pokemon.hunger--
+            })
+    },
+
+    //when clicked on play time decrease Boredom progress-bar by one
+    //when not clicked on play time increase progress-bar Boredom by one
+    //when Boredom progress bar reaches 10 kill pet(pokemon) pause video Show the result
+
+    /* ***** Play Time(boredom) ***** */
+    bored() {
+        setInterval(() => {
+                document.getElementById("boredom").innerText = `boredom: ${Math.ceil(pokemon.boredom /100 * 10)}`
+                document.getElementById("boredom").style.width = pokemon.boredom + "%";
+                pokemon.boredom++
+            }, 500),
+            document.querySelector(".playTimeBtn").addEventListener("click", () => {
+                pokemon.boredom--
+            })
+        if (pokemon.boredom == 1) {
+            document.getElementById("pichu").style.display = "none"
+            document.getElementById("pikachu").style.display = "none"
+            document.getElementById("raichu").style.display = "none"
+            document.getElementById("pokemonDead").style.display = "flex";
+         
+        }
+    }
+}
+
+
+
+
+
+
+
+document.querySelector(".lightsOnOffBtn").addEventListener("click", () => {
+    tamagotchi.lightsOnOff();
+    pokemon.sleep--
+
+})
+
+// document.getElementById("startBtn").addEventListener("click", () => {
+//     tamagotchi.ageUp();
+// })
 // Take the input and apply it to input-name h2
 // After clicking on submit close the modal-box(display:none) and change opacity of modal container to 0
 
@@ -55,49 +171,6 @@ document.querySelector(".close-btn").addEventListener("click", () => {
 
 /* ***************************************** Modal Section End ********************************************* */
 
-
-/* ***************************************** Controls Section start ********************************************* */
-
-//when clicked on play time decrease Boredom progress-bar by one
-//when not clicked on play time increase progress-bar Boredom by one
-//when Boredom progress bar reaches 10 kill pet(pokemon) pause video Show the result
-
-/* ***** Play Time(boredom) ***** */
-
-
-
-
-
-//when clicked on feed me decrease eat progress-bar by one
-//when not clicked on feed me increase progress-bar eat by one
-//when eat progress bar reaches 10 kill pet(pokemon) pause video Show the result
-
-/* ***** Feed Me(Eat) ***** */
-
-
-
-
-
-
-
-
-//when clicked on lights on/off button toggle video src to lights-on/out video according
-//when lights off change button to green, when lights on change button to red
-//when clicked on lights on/off descrease sleep progress-bar by one
-//when not-clicked on lights on/off increase sleep progress-bar by one
-//when progress bar reached 10 kill pet(pokemon) pause video Show the result
-
-/* ***** Lights on/off (sleep)***** */
-
-// function lightsOnOffToggle() {
-//     let element = document.querySelector(".btn-outline-success").toggleAttribute
-//     .
-// }
-
-
-
-/* ***************************************** Controls Section End ********************************************* */
-
 /* ***************************************** Game Logic Section Start ********************************************* */
 
 //When clicked on start button start timer for the pokemon to hatch after 10 seconds
@@ -105,32 +178,52 @@ document.querySelector(".close-btn").addEventListener("click", () => {
 
 /* ***** Start Button ***** */
 const startButton = {
-    pokeballOpen: document.querySelector(".btn-outline-danger").addEventListener("click", () => {
-        //playLightsOnVideo on click
-        document.getElementById("myVideo").style.display = "none"
-        document.getElementById("lightsOnVideo").style.display = "flex"
-        //onClick start button hide closed-pokeball & dislay open-pokeball after 8 seconds
-        setTimeout(() => {
-            document.getElementById("pichu").style.display = "flex"
-            document.getElementById("pokeball-closed").style.display = "none";
-            //display pikachu and hide pichu after 8 seconds + 10 seconds
-            let pikachuDisplay = setTimeout(() => {
-                    document.getElementById("pikachu").style.display = "flex";
-                    document.getElementById("pichu").style.display = "none";
-                    //display raichu and hide pikachu after 8 seconds + 10 seconds + 10 seconds
-                    let raichuDisplay = setTimeout(() => {
-                        document.getElementById("raichu").style.display = "flex";
-                        document.getElementById("pikachu").style.display = "none";
-                    }, 10000)
-                },
-                10000)
+    pokeballOpen() {
+        document.querySelector(".btn-outline-danger").addEventListener("click", (event) => {
+            //playLightsOnVideo on click
+            event.target.disabled = true;
+            // document.getElementById("myVideo").style.display = "none"
+            // document.getElementById("lightsOnVideo").style.display = "flex"
+            tamagotchi.ageUp();
+            tamagotchi.hungry();
+            tamagotchi.sleepUp();
+            tamagotchi.bored()
+                
+            
+            document.querySelector(".lightsOnOffBtn").addEventListener("click", () => {
+                lightsOn = !lightsOn;
+                if (lightsOn) {
+                    document.querySelector("#lightsOffVideo").src = "./images/lights-off.mp4"
+                    
+                } else if(lightsOn = !false ) {
+                    document.querySelector("#lightsOnVideo").src = "./images/lights-off.mp4"
+                    
+                }
 
-        }, 8000)
-    })
+            })
+            //onClick start button hide closed-pokeball & dislay pichu after 8 seconds
+            setTimeout(() => {
+                document.getElementById("pichu").style.display = "flex"
+                document.getElementById("pokeball-closed").style.display = "none";
+                //display pikachu and hide pichu after 8 seconds + 10 seconds
+                let pikachuDisplay = setTimeout(() => {
+                        document.getElementById("pikachu").style.display = "flex";
+                        document.getElementById("pichu").style.display = "none";
+                        //display raichu and hide pikachu after 8 seconds + 10 seconds + 10 seconds
+                        let raichuDisplay = setTimeout(() => {
+                            document.getElementById("raichu").style.display = "flex";
+                            document.getElementById("pikachu").style.display = "none";
+                        }, 10000)
+                    },
+                    10000)
+
+            }, 8000)
+        })
+    }
 }
 
 
-
+startButton.pokeballOpen();
 
 
 
